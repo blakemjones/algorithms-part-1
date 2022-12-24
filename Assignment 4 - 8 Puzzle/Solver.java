e8e9e5b0830a9ae81d2f3eef26e05571507ff24c
guillaumetine <blake.matthew.jones@gmail.com>
2022-12-24 11:42:48 -0700

    implemented twin search to detect if board is unsolvable

--- Assignment 4 - 8 Puzzle/Solver.java
+++ Assignment 4 - 8 Puzzle/Solver.java
@@ -1,38 +1,71 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
     private Board initial;
+    private Board initialTwin;
     private ArrayList<Board> movesToSolution;
+    private ArrayList<Board> twinMovesToSolution;
+    private boolean isSolvable;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         // initialize solver
-        this.movesToSolution = new ArrayList<Board>();
         this.initial = initial;
+        this.movesToSolution = new ArrayList<Board>();
+        this.initialTwin = initial.twin();
+        this.twinMovesToSolution = new ArrayList<Board>();
 
-        // initialize search queue
-        MinPQ<SearchNode> queue = new MinPQ<SearchNode>(new ManhattanPriority());
-        SearchNode initialSearchNode = new SearchNode(initial, 0, null);
-        queue.insert(initialSearchNode);
+        // initialize search queues
+        Comparator<SearchNode> comparator = new ManhattanPriority();
+        MinPQ<SearchNode> queue = initializeQueue(initial, comparator);
+        MinPQ<SearchNode> twinQueue = initializeQueue(initialTwin, comparator);
 
         // initialize search
-        SearchNode dequeued = queue.delMin();
-        movesToSolution.add(dequeued.board);
+        SearchNode dequeued = initializeSearch(queue, movesToSolution);
+        SearchNode twinDequeued = initializeSearch(twinQueue, twinMovesToSolution);
 
         // A* search
         while (!dequeued.board.isGoal()) {
+            // search
             for (Board neighbor : dequeued.board.neighbors()) {
                 if (neighbor.equals(dequeued.board)) continue; // check if neighbor is previous node
-                SearchNode newNode = new SearchNode(neighbor, dequeued.moves + 1, dequeued);
+                SearchNode newNode = new SearchNode(neighbor, dequeued.moves + 1);
                 queue.insert(newNode);
             }
             dequeued = queue.delMin(); // find board closest to solution
             movesToSolution.add(dequeued.board);
+
+            // twin search
+            if (twinDequeued.board.isGoal()) {
+                isSolvable = false;
+                return;
+            }
+            for (Board neighbor : twinDequeued.board.neighbors()) {
+                if (neighbor.equals(twinDequeued.board)) continue; // check if neighbor is previous node
+                SearchNode newNode = new SearchNode(neighbor, twinDequeued.moves + 1);
+                twinQueue.insert(newNode);
+            }
+            twinDequeued = twinQueue.delMin(); // find board closest to solution
         }
+        isSolvable = true;
+    }
+
+    private MinPQ<SearchNode> initializeQueue(Board board, Comparator<SearchNode> comparator) {
+        MinPQ<SearchNode> queue = new MinPQ<SearchNode>(comparator);
+        SearchNode initial = new SearchNode(board, 0);
+        queue.insert(initial);
+        return queue;
+    }
+
+    private SearchNode initializeSearch(MinPQ<SearchNode> queue, ArrayList<Board> movesToSolution) {
+        SearchNode dequeued = queue.delMin();
+        movesToSolution.add(dequeued.board);
+        return dequeued;
     }
 
     private class ManhattanPriority implements Comparator<SearchNode>
@@ -53,7 +86,7 @@ public class Solver {
         private Board board;
         private int moves;
 
-        public SearchNode(Board board, int moves, SearchNode previousNode) {
+        public SearchNode(Board board, int moves) {
             this.board = board;
             this.moves = moves;
         }
@@ -61,7 +94,7 @@ public class Solver {
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-        return true;
+        return isSolvable;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
@@ -78,13 +111,12 @@ public class Solver {
     public static void main(String[] args) {
 
         // create initial board from file
-        // In in = new In(args[0]);
-        // int n = in.readInt();
-        // int[][] tiles = new int[n][n];
-        // for (int i = 0; i < n; i++)
-        //     for (int j = 0; j < n; j++)
-        //         tiles[i][j] = in.readInt();
-        int [][] tiles = {{1, 2, 3},{0, 5, 6},{4, 7, 8}};
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
         Board initial = new Board(tiles);
     
         // solve the puzzle
